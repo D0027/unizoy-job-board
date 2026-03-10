4fe98fbfe72aa34a9afbb4bb76cbbd8acc067662
D0027 <dy1069489@gmail.com>
2026-03-10 17:41:00 +0530

    Remove duplicate lockfile for Vercel

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // Ignore type errors during build
-    ignoreBuildErrors: true,
-  },
+  /* Config options here */
 };
 
 export default nextConfig;
